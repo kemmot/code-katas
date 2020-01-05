20480d190dc8893890b59c492bc51dd624885c1a
K E Bradshaw <k.e.bradshaw@gmail.com>
2020-01-05 19:50:15 +0000

    Added faster solution for finding largest prime factors
    This is so that the value of 600 billion+ in the question can be checked
    in a reasonable amount of time.

--- project-euler/p003_largest_prime_factor.py
+++ project-euler/p003_largest_prime_factor.py
@@ -1,32 +1,54 @@
 import functools as ft
+import math
 
 
 class LargestPrimeFactor:
     def execute(self, value):
-        is_factor = ft.partial(self.is_factor, value=value)
-        primes = PrimeGenerator().generate(int(value / 2))
-        factors = filter(is_factor, primes)
-        return max(factors)
-
-    def is_factor(self, possible_factor, value):
-        return value % possible_factor == 0
-
-
-class PrimeGenerator:
-    def generate(self, max):
-        current_primes = []
-        for value in range(2, max + 1):
-            if self.is_prime(value, current_primes):
-                current_primes.append(value)
-                yield value
+        factors = FactorGenerator().get_factors(value)
+        prime_factors = filter(PrimeChecker().is_prime, factors)
+        return max(prime_factors)
     
-    def is_prime(self, value, current_primes):
-        if value == 1:
-            is_prime = False
-        else:
-            is_prime = True
-            for prime in current_primes:
-                if value % prime == 0:
+
+class FactorGenerator:
+    def get_factors(self, value):
+        factors = []
+        try:
+            starting_factor = self.get_starting_factor(value)
+
+            factors_to_try = []
+            factors_to_try.append(starting_factor)
+
+            while len(factors_to_try) > 0:
+                factor_to_try = factors_to_try.pop()
+                next_factor = int(value / factor_to_try)
+                if not next_factor in factors:
+                    factors_to_try.append(next_factor)
+                    factors.append(next_factor)
+                for next_factor in self.get_factors(factor_to_try):
+                    if not next_factor in factors:
+                        factors_to_try.append(next_factor)
+                        factors.append(next_factor)
+        except:
+            # prime factor reached
+            pass
+        return factors
+
+    def get_starting_factor(self, value):
+        possible_factor = 2
+        while possible_factor < value:
+            if value % possible_factor == 0:
+                return possible_factor
+            else:
+                possible_factor += 1
+        raise Exception('No starting factor found')
+
+
+class PrimeChecker:
+    def is_prime(self, value):
+        is_prime = True
+        if value != 2:
+            max_factor = math.ceil(math.sqrt(value))
+            for divisor in range(2, max_factor + 1):
+                if value % divisor == 0:
                     is_prime = False
-                    break
         return is_prime
--- project-euler/p003_largest_prime_factor_test.py
+++ project-euler/p003_largest_prime_factor_test.py
@@ -27,19 +27,72 @@ class ImplementationTests(unittest.TestCase):
 
     def test_largest_prime_factor_of_13195_is_29(self):
         self.execute_test(13195, 29)
+    
+    def test_largest_prime_factor_of_600851475143_is_29(self):
+        self.execute_test(600851475143, 6857)    
 
     def execute_test(self, value, expected_result):
         result = self.target.execute(value)
         self.assertEqual(expected_result, result)
 
 
-class PrimeGeneratorTests(unittest.TestCase):
-    def test_first_primes(self):
-        target = implementation.PrimeGenerator()
-        primes = list(target.generate(20))
-        self.assertEqual(2, primes[0])
-        self.assertEqual(3, primes[1])
-        self.assertEqual(5, primes[2])
-        self.assertEqual(7, primes[3])
-        self.assertEqual(11, primes[4])
-        self.assertEqual(13, primes[5])
+class FactorGeneratorTests(unittest.TestCase):
+    def test_factors_of_6(self):
+        factors = implementation.FactorGenerator().get_factors(6)
+        factors.sort()
+        self.assertEqual(2, len(factors))
+        self.assertEqual(2, factors[0])
+        self.assertEqual(3, factors[1])
+
+    def test_factors_of_12(self):
+        factors = implementation.FactorGenerator().get_factors(12)
+        factors.sort()
+        self.assertEqual(4, len(factors))
+        self.assertEqual(2, factors[0])
+        self.assertEqual(3, factors[1])
+        self.assertEqual(4, factors[2])
+        self.assertEqual(6, factors[3])
+
+    def test_factors_of_20(self):
+        factors = implementation.FactorGenerator().get_factors(20)
+        factors.sort()
+        self.assertEqual(4, len(factors))
+        self.assertEqual(2, factors[0])
+        self.assertEqual(4, factors[1])
+        self.assertEqual(5, factors[2])
+        self.assertEqual(10, factors[3])
+
+
+class PrimeCheckerTests(unittest.TestCase):
+    def test_is_2_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(2))
+
+    def test_is_3_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(3))
+
+    def test_is_4_prime(self):
+        self.assertFalse(implementation.PrimeChecker().is_prime(4))
+
+    def test_is_5_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(5))
+
+    def test_is_6_prime(self):
+        self.assertFalse(implementation.PrimeChecker().is_prime(6))
+
+    def test_is_7_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(7))
+
+    def test_is_10_prime(self):
+        self.assertFalse(implementation.PrimeChecker().is_prime(10))
+
+    def test_is_17_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(17))
+
+    def test_is_7918_prime(self):
+        self.assertFalse(implementation.PrimeChecker().is_prime(7918))
+
+    def test_is_7919_prime(self):
+        self.assertTrue(implementation.PrimeChecker().is_prime(7919))
+
+    def test_is_7920_prime(self):
+        self.assertFalse(implementation.PrimeChecker().is_prime(7920))
