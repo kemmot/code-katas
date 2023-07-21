155d9f88bf4bb132aeaacebf7d5a5e5448ec8a79
Karl Bradshaw <karl.bradshaw@wtwco.com>
2023-07-21 18:37:02 +0100

    Refactored day one to solve part 2

--- advent-of-code/2022/day01_calorie-counting.ps1
+++ advent-of-code/2022/day01_calorie-counting.ps1
@@ -1,41 +1,27 @@
 Param (
+    [int] $ElfCount = 3,
     [string] $InputPath = 'day01_input2.txt'
 )
-
-write-host $InputPath
 
 # TODO: consider a draw with multiple elves carrying a joint highest amount of calories
 
 $LineCounter = 1
 $ElfCounter = 1
 $CurrentElfCalories = 0
-$MostElfCalories = -1
-$ElfWithMostCalories = -1
-
-function Write-BestElf() {
-    Write-Host "Elf with most calories is elf $ElfWithMostCalories : $MostElfCalories calories"
-}
-
-function Check-Elf($Index, $CalorieCount) {
-    if ($CalorieCount -gt $MostElfCalories) {
-        $script:MostElfCalories = $CalorieCount
-        $script:ElfWithMostCalories = $Index
-        Write-BestElf
-    }
-}
 
 function Finish-Elf() {
-    Write-Verbose "Elf $ElfCounter complete : $CurrentElfCalories callories"
-    Check-Elf $ElfCounter $CurrentElfCalories
+    $Elf = New-Object PSObject -Property:@{ 'Index'=$ElfCounter; 'Calories'=$CurrentElfCalories }
+    Write-Verbose "Elf $($Elf.Index) complete : $($Elf.Calories) callories"
     $script:CurrentElfCalories = 0
     $script:ElfCounter++
+    return $Elf
 }
 
-Get-Content $InputPath | %{
+$Elves = Get-Content $InputPath | %{
     Write-Debug "$LineCounter - $_"
     
     if ($_ -eq '') {
-        Finish-Elf
+        Write-Output (Finish-Elf)
     } else {
         $CurrentElfCalories += [int]$_
         Write-Debug "Added $_ to elf $ElfCounter, now at $CurrentElfCalories"
@@ -45,7 +31,7 @@ Get-Content $InputPath | %{
 }
 
 if ($CurrentElfCalories -gt 0) {
-    Finish-Elf
+    $Elves += (Finish-Elf)
 }
 
-Write-BestElf
+$Elves | Sort-Object Calories -Descending | Select-Object -First $ElfCount | Measure-Object Calories -Sum
